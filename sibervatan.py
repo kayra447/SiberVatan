a9fcd55f0f27cf9ed90a4788db7c8fbb30030588
kayra447 <155827771+kayra447@users.noreply.github.com>
2024-01-10 19:10:04 +0300

    first commit
    siber vatan python örnek

---
+++ sibervatan.py
@@ -0,0 +1,17 @@
+from math import sqrt
+sayi= -7
+print("mutlak deger", abs(sayi))
+
+us=pow(2,3)
+print("girdiğiniz sayının üssü:",us)
+
+sayi1= 36
+karekok= sqrt(sayi1)
+print("sayinin karekoku",karekok)
+
+from math import ceil,floor
+ondalik=10.25
+yukari=ceil (ondalik)
+asagi=floor (ondalik)
+print("yukari", yukari)
+print("asagi",asagi)
